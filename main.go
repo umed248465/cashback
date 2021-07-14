d7a2d92651f2b1dd80e8042f94b8f617f78f7ca6
umed <umed4878@gmail.com>
2021-07-14 20:02:12 +0500

    homework

--- main.go
+++ main.go
@@ -3,7 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	//fmt.Println("Hello" )
+	minAmount := 5000
+	purchase := 15000
+	percentCashback := 15
+	cashbackLimit := 2000
+	cashback := 0
+	const fullPercent = 100
+
+	if purchase >= minAmount{
+		cashback = purchase * percentCashback / fullPercent
+		fmt.Println("Prediction cashback is", cashback)
+	}
+	if cashback > cashbackLimit{
+		cashback = cashbackLimit
+	}
+	fmt.Println("cashback is", cashback)
 }
 
 //DRAFT
